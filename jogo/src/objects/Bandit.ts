429478889c5f3923c51038755cfc69755b26c89a
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-07-18 18:21:55 -0300

    tentando fazer animação

--- jogo/src/objects/Bandit.ts
+++ jogo/src/objects/Bandit.ts
@@ -1,9 +1,8 @@
 import { Scene } from 'phaser';
-import image from '/assets/Axe_Bandit.png'
 
 export default class Bandit extends Phaser.GameObjects.Sprite {
   static readonly bandit = 'Bandit';
-  static readonly srcImage = image;
+  static readonly srcImage = '/assets/axe_bandit_run.png';
   static readonly move = 8;
 
   constructor(scene: Scene, x: number, y: number) {
@@ -17,10 +16,22 @@ export default class Bandit extends Phaser.GameObjects.Sprite {
       frameWidth: 80,
       frameHeight: 80,
     });
+
+    scene.anims.create({
+      key: 'walk',
+      frames: scene.anims.generateFrameNumbers('bandit_walk', {
+        frames: [1, 2, 3, 4, 5, 6, 7, 8],
+      }),
+      frameRate: 10,
+      duration: 1000,
+      repeat: -1,
+    });
   }
 
   move(keypressed: Phaser.Types.Input.Keyboard.CursorKeys): void {
     if (keypressed.up.isDown) {
+      console.log('up');
+      
       this.keyUp();
     }
     if (keypressed.left.isDown) {
@@ -35,15 +46,17 @@ export default class Bandit extends Phaser.GameObjects.Sprite {
   }
 
   keyUp(): void {
-    this.y -= Bandit.move;
+    this.anims.play('walk');
+    this.scene.anims.play('walk', this);
+    // this.y -= Bandit.move;
   }
   keyLeft(): void {
-    this.x -= Bandit.move;
+    // this.x -= Bandit.move;
   }
   keyDown(): void {
-    this.y += Bandit.move;
+    // this.y += Bandit.move;
   }
   keyRight(): void {
-    this.x += Bandit.move;
+    // this.x += Bandit.move;
   }
 }
