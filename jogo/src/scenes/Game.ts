275d2f7f30dcbe0cbb8e5625e3ca540002562030
Ellian Aragão Dias <ellian.dias@capgemini.com>
2021-07-20 21:55:13 -0300

    sprite de animação funcionando

--- jogo/src/scenes/Game.ts
+++ jogo/src/scenes/Game.ts
@@ -1,26 +1,33 @@
-import Phaser from 'phaser';
-import Bandit from '../objects/Bandit';
+import Phaser from "phaser";
 
 export default class Demo extends Phaser.Scene {
-  private bandit: Bandit;
-  private cursorKeysPressed: Phaser.Types.Input.Keyboard.CursorKeys;
-  
+  bandit: any;
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   preload() {
-    this.load.image('logo', 'assets/sample1.png');
-    Bandit.preload(this);
+    this.load.image("logo", "assets/sample1.png");
+    this.bandit = this.load.spritesheet("bandit", "assets/axe_bandit_run.png", {
+      frameWidth: 80,
+      frameHeight: 80,
+    });
     this.cursorKeysPressed = this.input.keyboard.createCursorKeys();
   }
 
   create() {
-    this.add.image(400, 250, 'logo');
-    this.bandit = this.add.existing(new Bandit(this, 450, 400));
-  }
+    this.add.image(400, 250, "logo");
+    this.bandit = this.add.sprite(450, 400, "bandit");
 
-  update(): void {
-    this.bandit.move(this.cursorKeysPressed);
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("bandit", {
+        frames: [0, 1, 2, 3, 4, 5, 6],
+      }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    this.bandit.play('run');
   }
 }
